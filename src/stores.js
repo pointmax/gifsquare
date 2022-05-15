a33841e0d0980943c53620f1e3fe5003709123de
Max Pointinger <max.pointinger@gmail.com>
2022-05-15 15:43:09 +0200

    feat: save interval in localStorage

--- src/stores.js
+++ src/stores.js
@@ -2,6 +2,10 @@ import { writable } from 'svelte/store';
 
 export let imagesArray = writable([]);
 
-export let interval = writable(1);
+const storedInterval = typeof localStorage !== 'undefined' && localStorage.interval;
+export let interval = writable(storedInterval || 1);
+interval.subscribe((value) =>
+	typeof localStorage !== 'undefined' ? (localStorage.interval = value) : ''
+);
 
 export let gifSrc = writable(null);
