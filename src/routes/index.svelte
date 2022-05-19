984d64acf0d4f45f2c919ba585d8b5ece0a14adf
Max Pointinger <max.pointinger@gmail.com>
2022-05-19 23:35:02 +0200

    fix: title and description not showing up

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,11 +6,6 @@
 	import Upload from '../components/Upload.svelte';
 </script>
 
-<svelte:head>
-	<title>GifSquare</title>
-	<meta name="description" content="Create square gifs like a breeze." />
-</svelte:head>
-
 <div class="flex py-4 min-h-[80vh] items-center justify-center">
 	<div
 		class="bg-white bg-opacity-50 rounded-lg shadow-md flex-grow container max-w-3xl items-center justify-center flex flex-col py-8 px-4 min-h-[30rem] mx-4 gap-4"
